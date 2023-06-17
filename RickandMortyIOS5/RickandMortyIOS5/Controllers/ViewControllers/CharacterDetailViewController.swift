b2f37435dd0767284557d799f12c1a4cbc9164f9
Colton Brenneman <colton.brenneman@gmail.com>
2023-06-16 23:38:42 -0600

    finished main. added viewControllers and subclassed properly

---
+++ RickandMortyIOS5/RickandMortyIOS5/Controllers/ViewControllers/CharacterDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CharacterDetailViewController.swift
+//  RickandMortyIOS5
+//
+//  Created by Colton Brenneman on 6/16/23.
+//
+
+import UIKit
+
+class CharacterDetailViewController: UIViewController {
+
+    // MARK: - Outlets
+    
+    @IBOutlet weak var characterSearchBar: UISearchBar!
+    
+    @IBOutlet weak var characterNameTableView: UITableView!
+    
+} //End of class
---
+++ RickandMortyIOS5/RickandMortyIOS5/Controllers/ViewControllers/CharacterListTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CharacterListTableViewCell.swift
+//  RickandMortyIOS5
+//
+//  Created by Colton Brenneman on 6/16/23.
+//
+
+import UIKit
+
+class CharacterListTableViewCell: UITableViewCell {
+
+    // MARK: - Outlets
+    
+    @IBOutlet weak var characterNameLabel: UILabel!
+    
+    @IBOutlet weak var characterIDLabel: UILabel!
+    
+    @IBOutlet weak var characterImageView: UIImageView!
+    
+} //End of class
